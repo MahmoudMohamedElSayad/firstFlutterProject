c81746808c6f0e7732a45f9074cf0862cbb05d59
codeLop-6 <mahmodb29@yahoo.com>
2022-10-13 16:44:41 +0200

    v3

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 import 'dart:ffi';
 
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -8,6 +7,8 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
+  TextEditingController nameController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -23,139 +24,129 @@ class HomePage extends StatelessWidget {
     return Scaffold(
         backgroundColor: Colors.white,
         appBar: AppBar(
-            backgroundColor: Colors.red,
-            title: Text("hello"),
+            backgroundColor: Colors.white,
+            title: const Text(
+              "الصفحة الرئيسية",
+              style: TextStyle(
+                  color: Colors.pink,
+                  fontWeight: FontWeight.bold,
+                  fontSize: 20,
+                  shadows: [
+                    Shadow(
+                        color: Colors.black,
+                        offset: Offset(0, 10),
+                        blurRadius: 20),
+                  ]),
+            ),
             titleTextStyle: const TextStyle(color: Colors.blue),
             centerTitle: true),
         drawer: const Drawer(),
-        body: Container(
-            child: SingleChildScrollView(
+        body: SingleChildScrollView(
           child: Column(
             children: [
               Card(
-                  margin: EdgeInsets.only(top: 20, right: 20, left: 20),
+                  margin: const EdgeInsets.only(top: 20, right: 8, left: 8),
                   color: Colors.white,
                   shadowColor: Colors.black,
                   elevation: 5,
                   shape: RoundedRectangleBorder(
                       borderRadius: BorderRadius.circular(20),
-                      side: BorderSide(color: Colors.red, width: 1)),
-                  child: Container(
-                    height: 500,
-                    child: Column(
-                      children: [
-                        Container(
-                          width: 500,
-                          height: 100,
-                          padding: EdgeInsets.only(top: 20),
-                          child: const Text(
-                            "بسم الله الرحمن الرحيم",
-                            textAlign: TextAlign.center,
-                            style: TextStyle(
-                                color: Colors.black,
-                                fontWeight: FontWeight.bold,
-                                fontSize: 40,
-                                shadows: <Shadow>[
-                                  Shadow(
-                                      color: Colors.red,
-                                      offset: Offset(0, 10),
-                                      blurRadius: 30)
-                                ]),
-                          ),
+                      side: const BorderSide(color: Colors.black, width: 1)),
+                  child: Column(
+                    children: [
+                      Container(
+                        width: 500,
+                        height: 100,
+                        padding: const EdgeInsets.only(top: 20),
+                        child: const Text(
+                          "عمل حساب جديد",
+                          textAlign: TextAlign.center,
+                          style: TextStyle(
+                              color: Colors.black,
+                              fontWeight: FontWeight.bold,
+                              fontSize: 40,
+                              shadows: <Shadow>[
+                                Shadow(
+                                    color: Colors.red,
+                                    offset: Offset(0, 10),
+                                    blurRadius: 30)
+                              ]),
                         ),
-                        Container(
-                          width: 500,
-                          height: 100,
-                          padding: EdgeInsets.only(top: 20),
-                          child: const Text(
-                            "بسم الله الرحمن الرحيم",
-                            textAlign: TextAlign.center,
-                            style: TextStyle(
-                                color: Colors.black,
-                                fontWeight: FontWeight.bold,
-                                fontSize: 40,
-                                shadows: <Shadow>[
-                                  Shadow(
-                                      color: Colors.red,
-                                      offset: Offset(0, 10),
-                                      blurRadius: 30)
-                                ]),
-                          ),
+                      ),
+                      Container(
+                        margin:
+                            const EdgeInsets.only(top: 5, right: 8, left: 8),
+                        child: TextField(
+                          style: const TextStyle(
+                              fontWeight: FontWeight.bold, fontSize: 15),
+                          maxLength: 50,
+                          maxLines: 1,
+                          decoration: InputDecoration(
+                              border: OutlineInputBorder(
+                                  borderRadius: BorderRadius.circular(10)),
+                              labelText: 'Full Name',
+                              focusColor: Colors.black),
                         ),
-                        Container(
-                          width: 500,
-                          height: 100,
-                          padding: EdgeInsets.only(top: 20),
-                          child: const Text(
-                            "بسم الله الرحمن الرحيم",
-                            textAlign: TextAlign.center,
-                            style: TextStyle(
-                                color: Colors.black,
-                                fontWeight: FontWeight.bold,
-                                fontSize: 40,
-                                shadows: <Shadow>[
-                                  Shadow(
-                                      color: Colors.red,
-                                      offset: Offset(0, 10),
-                                      blurRadius: 30)
-                                ]),
-                          ),
+                      ),
+                      Container(
+                        margin:
+                            const EdgeInsets.only(top: 2, right: 8, left: 8),
+                        child: TextField(
+                          style: const TextStyle(
+                              fontWeight: FontWeight.bold, fontSize: 15),
+                          maxLength: 50,
+                          maxLines: 1,
+                          decoration: InputDecoration(
+                              border: OutlineInputBorder(
+                                  borderRadius: BorderRadius.circular(10)),
+                              labelText: 'Last Name',
+                              focusColor: Colors.black),
                         ),
-                        Container(
-                          width: 500,
-                          height: 100,
-                          padding: EdgeInsets.only(top: 20),
-                          child: const Text(
-                            "بسم الله الرحمن الرحيم",
-                            textAlign: TextAlign.center,
-                            style: TextStyle(
-                                color: Colors.black,
-                                fontWeight: FontWeight.bold,
-                                fontSize: 40,
-                                shadows: <Shadow>[
-                                  Shadow(
-                                      color: Colors.red,
-                                      offset: Offset(0, 10),
-                                      blurRadius: 30)
-                                ]),
-                          ),
+                      ),
+                      Container(
+                        margin:
+                            const EdgeInsets.only(top: 2, right: 8, left: 8),
+                        child: TextField(
+                          maxLength: 50,
+                          maxLines: 1,
+                          decoration: InputDecoration(
+                              border: OutlineInputBorder(
+                                  borderRadius: BorderRadius.circular(10)),
+                              labelText: 'Password',
+                              focusColor: Colors.black),
                         ),
-                        Container(
-                          width: 500,
-                          height: 100,
-                          padding: EdgeInsets.only(top: 20),
-                          child: const Text(
-                            "بسم الله الرحمن الرحيم",
-                            textAlign: TextAlign.center,
-                            style: TextStyle(
-                                color: Colors.black,
-                                fontWeight: FontWeight.bold,
-                                fontSize: 40,
-                                shadows: <Shadow>[
-                                  Shadow(
-                                      color: Colors.red,
-                                      offset: Offset(0, 10),
-                                      blurRadius: 30)
-                                ]),
-                          ),
-                        )
-                      ],
-                    ),
+                      ),
+                      Container(
+                        margin: const EdgeInsets.only(
+                            top: 2, right: 8, left: 8, bottom: 20),
+                        child: TextField(
+                          style: const TextStyle(
+                              fontWeight: FontWeight.bold, fontSize: 15),
+                          maxLength: 50,
+                          maxLines: 1,
+                          decoration: InputDecoration(
+                              border: OutlineInputBorder(
+                                  borderRadius: BorderRadius.circular(10)),
+                              labelText: 'Confirm Password',
+                              focusColor: Colors.black),
+                        ),
+                      ),
+                    ],
                   )),
               Card(
-                  margin: EdgeInsets.only(top: 20, right: 20, left: 20),
+                  margin: const EdgeInsets.only(top: 20, right: 20, left: 20),
                   color: Colors.white,
                   shadowColor: Colors.black,
                   elevation: 5,
                   shape: RoundedRectangleBorder(
                       borderRadius: BorderRadius.circular(20),
-                      side: BorderSide(color: Colors.red, width: 1)),
+                      side: const BorderSide(color: Colors.red, width: 1)),
                   child: Column(
                     children: [
                       Container(
                         width: 500,
                         height: 200,
-                        padding: EdgeInsets.only(top: 20),
+                        padding: const EdgeInsets.only(top: 20),
                         child: const Text(
                           "بسم الله الرحمن الرحيم",
                           textAlign: TextAlign.center,
@@ -174,7 +165,7 @@ class HomePage extends StatelessWidget {
                       Container(
                         width: 500,
                         height: 200,
-                        padding: EdgeInsets.only(top: 10),
+                        padding: const EdgeInsets.only(top: 10),
                         child: const Text(
                           "بسم الله الرحمن الرحيم",
                           textAlign: TextAlign.center,
@@ -193,17 +184,17 @@ class HomePage extends StatelessWidget {
                     ],
                   )),
               Card(
-                  margin: EdgeInsets.only(top: 20, right: 20, left: 20),
+                  margin: const EdgeInsets.only(top: 20, right: 20, left: 20),
                   color: Colors.white,
                   shadowColor: Colors.black,
                   elevation: 5,
                   shape: RoundedRectangleBorder(
                       borderRadius: BorderRadius.circular(20),
-                      side: BorderSide(color: Colors.red, width: 1)),
+                      side: const BorderSide(color: Colors.red, width: 1)),
                   child: Container(
                     width: 500,
                     height: 200,
-                    padding: EdgeInsets.only(top: 20),
+                    padding: const EdgeInsets.only(top: 20),
                     child: const Text(
                       "بسم الله الرحمن الرحيم",
                       textAlign: TextAlign.center,
@@ -220,18 +211,18 @@ class HomePage extends StatelessWidget {
                     ),
                   )),
               Card(
-                  margin:
-                      EdgeInsets.only(top: 20, right: 20, left: 20, bottom: 20),
+                  margin: const EdgeInsets.only(
+                      top: 20, right: 20, left: 20, bottom: 20),
                   color: Colors.white,
                   shadowColor: Colors.black,
                   elevation: 5,
                   shape: RoundedRectangleBorder(
                       borderRadius: BorderRadius.circular(20),
-                      side: BorderSide(color: Colors.red, width: 1)),
+                      side: const BorderSide(color: Colors.red, width: 1)),
                   child: Container(
                     width: 500,
                     height: 200,
-                    padding: EdgeInsets.only(top: 20),
+                    padding: const EdgeInsets.only(top: 20),
                     child: const Text(
                       "بسم الله الرحمن الرحيم",
                       textAlign: TextAlign.center,
@@ -249,17 +240,6 @@ class HomePage extends StatelessWidget {
                   )),
             ],
           ),
-        )));
-  }
-}
-
-class Login extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(),
-      drawer: const Drawer(),
-      body: const Text("login"),
-    );
+        ));
   }
 }
